62ae630f5e037b1bfdf3228db9d2b88736d080a2
Lochmatter Christian <christian.lochmatter@gmx.ch>
2010-11-23 20:12:41 +0100

    1st running version

---
+++ src/com/clo/cota/cotaItemDetail.java
@@ -0,0 +1,39 @@
+package com.clo.cota;
+
+import java.util.Set;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class cotaItemDetail extends Activity {
+
+	private TextView fullname = null;
+	private TextView firma = null;
+	private TextView adresse = null;
+	private TextView plzort = null;
+	private TextView email = null;
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_detail);
+        fullname = (TextView) findViewById(R.id.TextView_vollname);
+        firma =  (TextView) findViewById(R.id.TextView_Firma);
+        email =  (TextView) findViewById(R.id.TextView_email);
+        plzort =  (TextView) findViewById(R.id.TextView_plz_ort);
+        adresse = (TextView) findViewById(R.id.TextView_adresse);
+        fillItems();
+    }
+    
+    private void fillItems(){
+    	Bundle b = getIntent().getExtras();
+		fullname.setText(b.getString("firstname") + " " + b.getString("lastname"));
+		firma.setText(b.getString("firma"));
+		email.setText(b.getString("email"));
+		plzort.setText(b.getString("zip") + " " + b.getString("city"));
+		adresse.setText(b.getString("address"));
+    }
+	
+}
---
+++ src/com/clo/cota/sax/AddressDetailSaxHandler.java
@@ -0,0 +1,79 @@
+package com.clo.cota.sax;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import com.clo.cota.entity.User;
+
+public class AddressDetailSaxHandler extends DefaultHandler {
+	  private User user = null;
+	  private String element = null;
+	
+	  @Override
+	  public void startElement(String ns,
+              String localName,
+              String qName,
+              Attributes attrs) throws SAXException {
+		  System.out.println("STARTELEMENT: " + localName);
+		  element = localName;
+		  if (localName.equals("user")){
+			  user = new User();
+		  }
+	  }
+
+	  @Override
+	  public void characters(char[] ch, int start, int length)
+	          throws SAXException {
+		  String value = new String(ch, start, length);
+		  System.out.println(element + "=" + value); 
+		  if (element.equals("Vorname")){
+			  user.setFirstname(value);
+			  element = "";
+		  }else if (element.equals("Name")){
+			  user.setLastname(value);	
+			  element = "";
+		  }else if (element.equals("PersonendatenID")){
+			  System.out.println("PersonendatenID: " + value);
+			  user.setId(new Integer(value));
+			  element = "";
+		  }else if (element.equals("email")){
+			  System.out.println("E-Mail: " + value);
+			  user.setEmail(value);
+			  element = "";
+		  }else if (element.equals("firma")){
+			  System.out.println("Firma: " + value);
+			  user.setFirma(value);
+			  element = "";
+		  }else if (element.equals("adresse")){
+			  System.out.println("Adrese: " + value);
+			  user.setAddress(value);
+			  element = "";
+		  }else if (element.equals("ort")){
+			  System.out.println("Ort: " + value);
+			  user.setCity(value);
+			  element = "";
+		  }else if (element.equals("plz")){
+			  System.out.println("PLZ: " + value);
+			  user.setPlz(value);
+			  element = "";
+		  }
+		  
+		  
+	  }
+
+	  @Override
+	  public void endElement(String ns, String localName, String qName)
+	          throws SAXException {
+		  System.out.println("ENDELEMENT: " + localName);
+	  }
+	  
+	  public User getUser() {
+		  return user;
+	  }
+
+	  public void setUser(User user) {
+		  this.user = user;
+	  }
+	  
+}
